0baf923c368d7d9b1b479d55dfd00835f7b9acc3
Mayank Verma <mayankverr@gmail.com>
2020-02-21 01:18:18 +0530

    make it more clear

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -8,21 +8,21 @@ const SearchBar = styled.div`
   align-items: center;
 `;
 
-const Search = styled.input`
+const SearchInput = styled.input`
   width: 270px;
   height: 34px;
   border-style: none;
   border-radius: 17px 0 0 17px;
   outline: none;
   padding: 0 8px 0 15px;
-  font-family: 'Open Sans', sans-serif;
+  font-family: 'Rubik', sans-serif;
   font-size: 14px;
 `;
 
 const searchBar = () => {
   return (
     <SearchBar>
-      <Search type='text' placeholder='Search...' />
+      <SearchInput type='text' placeholder='Search...' />
       <SearchIcon width='18px' height='18px' fill='#121212' />
     </SearchBar>
   );
