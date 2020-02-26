591ec3e8b59c74530c6e6223287cf086bc49d443
Mayank Verma <mayankverr@gmail.com>
2020-02-26 22:12:36 +0530

    make it a button

--- components/icons/SearchIcon.js
+++ components/icons/SearchIcon.js
@@ -1,18 +1,21 @@
 import styled from 'styled-components';
 
-const Div = styled.div`
+const Btn = styled.button`
   background-color: #fff;
   display: flex;
   justify-content: center;
   align-items: center;
   width: 34px;
   height: 34px;
+  border: 0;
   border-radius: 0 5px 5px 0;
+  cursor: pointer;
+  outline: none;
 `;
 
 const searchIcon = props => {
   return (
-    <Div>
+    <Btn onClick={props.click}>
       <svg
         xmlns='http://www.w3.org/2000/svg'
         width={props.width}
@@ -22,7 +25,7 @@ const searchIcon = props => {
       >
         <path d='m378.344 332.78c25.37-34.645 40.545-77.2 40.545-123.333 0-115.484-93.961-209.445-209.445-209.445s-209.444 93.961-209.444 209.445 93.961 209.445 209.445 209.445c46.133 0 88.692-15.177 123.337-40.547l137.212 137.212 45.564-45.564c0-.001-137.214-137.213-137.214-137.213zm-168.899 21.667c-79.958 0-145-65.042-145-145s65.042-145 145-145 145 65.042 145 145-65.043 145-145 145z' />
       </svg>
-    </Div>
+    </Btn>
   );
 };
 
